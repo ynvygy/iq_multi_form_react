f35e9b932d866169bad2f029976dc6c277e33068
ynvygy <lugerand2004@yahoo.com>
2017-09-25 18:51:51 +0200

    added IQ class skeleton

--- src/Iq.js
+++ src/Iq.js
@@ -13,7 +13,46 @@ var config = {
 firebase.initializeApp(config);
 
 class IQ extends Component{
+	userSubmit(event){
+		var username = this.refs.name.value;
+		this.setState({username: username})
+	}
+
+	constructor(props){
+		super(props)
+
+		this.state ={
+			uid: uuid.v1(),
+			username: '',
+			answers: {
+				ans1: '',
+				ans2: '',
+				ans3: '',
+				ans4: '',
+				ans5: '',
+				ans6: ''
+			},
+			isSubmitted: false
+		}
+
+	this.userSubmit = this.userSubmit.bind(this)
+	}
 	render(){
+		var username;
+		var questions;
+
+		if(this.state.username==='' && this.state.isSubmitted === false){
+			username = <div>
+				<h1>hello user please tell us your name</h1>
+					<form onSubmit={this.userSubmit}>
+						<input className="username" type="text" placeholder="enter your name" ref="name" />
+					</form>
+			</div>
+			questions = ''
+		} else if (this.state.username !== '' && this.state.isSubmitted === false )	{
+			username = <h1> Welcome {this.state.username}</h1>
+		}
+
 		return(
 			<div>
 				Initial test
